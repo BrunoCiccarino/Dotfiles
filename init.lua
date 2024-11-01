a080cd08d8469f8119eb6bdb15e1b022c91d542d
BrunoCiccarino <landneco0@gmail.com>
2024-11-01 12:52:38 -0300

    feat(config): adding plugins to customize the command line

--- init.lua
+++ init.lua
@@ -1,11 +1,34 @@
 vim.cmd [[
-  call plug#begin('~/.local/share/nvim/plugged')
-    Plug 'neovim/nvim-lspconfig',
-    Plug 'ellisonleao/gruvbox.nvim',
-    Plug 'nvim-telescope/telescope.nvim', { 'branch': '0.1.x' }
-    Plug 'nvim-treesitter/nvim-treesitter', { 'do': ':TSUpdate' }
-  call plug#end()
+call plug#begin('~/.local/share/nvim/plugged')
+
+Plug 'neovim/nvim-lspconfig'
+Plug 'ellisonleao/gruvbox.nvim'
+Plug 'nvim-telescope/telescope.nvim', { 'branch': '0.1.x' }
+Plug 'nvim-treesitter/nvim-treesitter', { 'do': ':TSUpdate' }
+Plug 'MunifTanjim/nui.nvim'         
+Plug 'rcarriga/nvim-notify'          
+Plug 'folke/noice.nvim'              
+
+call plug#end()
 ]]
+
+
+require("noice").setup({
+    cmdline = {
+        enabled = true,
+        view = "cmdline_popup",  
+        format = {
+            cmdline = { icon = ">" },
+        },
+    },
+    popupmenu = {
+        enabled = true,
+    },
+    messages = {
+        enabled = false,
+    },
+})
+
 
 vim.o.number = true
 vim.o.mouse = "a"
