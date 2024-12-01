44481749995233636e1bee8b862df5c1dad894ea
BrunoCiccarino <landneco0@gmail.com>
2024-11-30 21:48:31 -0300

    fix mappings

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,36 @@
+local map = vim.keymap.set
+
+map("n", "<C-s>", "<cmd> w <CR>")
+map("i", "jk", "<ESC>")
+map("n", "<C-c>", "<cmd> %y+ <CR>")
+
+-- nvimtree
+map("n", "<C-n>", "<cmd> NvimTreeToggle <CR>")
+map("n", "<C-h>", "<cmd> NvimTreeFocus <CR>")
+
+-- telescope
+map("n", "<leader>ff", "<cmd> Telescope find_files <CR>")
+map("n", "<leader>fo", "<cmd> Telescope oldfiles <CR>")
+map("n", "<leader>fw", "<cmd> Telescope live_grep <CR>")
+map("n", "<leader>gt", "<cmd> Telescope git_status <CR>")
+
+-- bufferline, cycle buffers
+map("n", "<Tab>", "<cmd> BufferLineCycleNext <CR>")
+map("n", "<S-Tab>", "<cmd> BufferLineCyclePrev <CR>")
+map("n", "<C-q>", "<cmd> bd <CR>")
+
+-- comment.nvim
+map("n", "<leader>/", "gcc", { remap = true })
+map("v", "<leader>/", "gc", { remap = true })
+
+-- format
+map("n", "<leader>fm", function()
+  require("conform").format()
+end)
+
+-- require("themesw")
+-- map("n", "<C-t>", ":lua SwitchTheme()<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<Leader>th', ':split | terminal<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>tv', ':vsplit | terminal<CR>', { noremap = true, silent = true })
+
