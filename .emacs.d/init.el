df4243c4fcf44fb9f987e7fdbd2f804809c22185
BrunoCiccarino <landneco0@gmail.com>
2024-12-02 02:13:47 -0300

    feat(emacs): adding my config for emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,161 @@
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+(package-refresh-contents)
+(package-install 'doom-themes)
+
+(unless (package-installed-p 'hydra)
+  (package-refresh-contents)
+  (package-install 'hydra))
+
+(load-theme 'doom-shades-of-purple t)
+
+(require 'hydra)
+
+(defun safe-load-theme (theme)
+  "Load THEME only if it's available."
+  (if (member theme (custom-available-themes))
+      (load-theme theme t)
+    (message "Theme %s not found" theme)))
+
+(defhydra load-themes (:color blue)
+  "load themes"
+  ("1" (load-theme 'doom-shades-of-purple t) "doom shades of purple")
+  ("2" (load-theme 'doom-snazzy t) "doom-snazzy")
+  ("3" (load-theme 'doom-dracula t) "doom-dracula")
+  ("4" (load-theme 'doom-spacegrey t) "doom-spacegrey")
+  ("q" nil "quit"))
+
+(global-set-key (kbd "C-c b") 'load-themes/body)
+
+(set-fringe-mode 15)
+(electric-indent-mode 1)
+(column-number-mode t)
+(global-display-line-numbers-mode t)
+(global-hl-line-mode t)
+(global-visual-line-mode t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+
+(setq inhibit-startup-message t)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+(setq auto-mode-alist (append '(("\\.scm$" . scheme-mode)) auto-mode-alist))
+(setq auto-mode-alist (append '(("\\.lisp$" . lisp-mode)) auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.el\\'" . emacs-lisp-mode))
+
+(use-package all-the-icons
+  :ensure t)
+
+(define-prefix-command 'my-leader-map)
+(global-set-key (kbd "\\") 'my-leader-map)
+(global-set-key (kbd "C-c C-c") 'kill-ring-save) ;; Shortcut to copy   
+(global-set-key (kbd "C-c C-a") 'set-mark-command) ;; Shortcut to select multiple lines (C-c C-a)
+(global-set-key (kbd "C-c C-v") 'yank) ;; Shortcut to paste (C-c C-v)
+(global-set-key (kbd "C-c C-s") 'save-buffer) ;; Shortcut to save (C-c C-s) 
+
+
+(define-key my-leader-map (kbd "f f") 'treemacs)     
+(define-key my-leader-map (kbd "b b") 'switch-to-buffer) 
+(define-key my-leader-map (kbd "w s") 'eshell) 
+
+(use-package vertico
+  :init
+  (vertico-mode))
+
+(use-package vertico-posframe
+  :after vertico
+  :custom
+  (vertico-posframe-parameters
+   '((left-fringe . 8)
+     (right-fringe . 8)))
+  :config
+  (vertico-posframe-mode 1))
+
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((python-mode . lsp))
+  :commands lsp)
+
+(use-package lsp-ui
+  :commands lsp-ui-mode)
+
+(use-package lsp-pyright
+  :after lsp-mode
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-pyright)
+                         (lsp))))
+
+(use-package lsp-mode
+  :ensure t
+  :hook ((lsp-mode . lsp-enable-which-key-integration))
+  :config (setq lsp-completion-enable-additional-text-edit nil))
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :ensure t)
+(use-package ccls
+  :ensure t
+  :config
+  (setq ccls-executable "ccls")
+  (setq lsp-prefer-flymake nil)
+  (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+  :hook ((c-mode c++-mode objc-mode) .
+         (lambda () (require 'ccls) (lsp))))
+(use-package flycheck
+  :ensure t)
+(use-package yasnippet
+  :ensure t
+  :config (yas-global-mode))
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+(use-package helm-lsp
+  :ensure t)
+(use-package helm
+  :ensure t
+  :config (helm-mode))
+(use-package lsp-treemacs
+  :ensure t)
+
+;;; This will enable emacs to compile a simple cpp single file without any makefile by just pressing [f9] key
+(defun code-compile()
+  (interactive)
+  (unless (file-exists-p "Makefile")
+    (set (make-local-variable 'compile-command)
+	 (let ((file (file-name-nondirectory buffer-file-name)))
+	   (format "%s -o %s %s"
+		   (if (equal (file-name-extension file) "cpp") "g++" "gcc")
+		   (file-name-sans-extension file)
+		   file)))
+    (compile compile-command)))
+(global-set-key [f9] 'code-compile)
+
+;; Configurar teclas globais
+(global-set-key (kbd "C-c C-e") 'eval-last-sexp)
+(global-set-key (kbd "C-c C-r") 'eval-region)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-pyright lsp-ui lsp-mode vertico-posframe vertico treemacs all-the-icons doom-themes hydra)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
